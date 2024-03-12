f790c44bc32b25e31d406ba57ce5f33eec84d141
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2024-03-12 15:11:34 -0500

    first commit

---
+++ demo.rb
@@ -0,0 +1,10 @@
+require_relative 'lib/celularazul'
+
+# require 'fleteo'
+
+def lambda
+  include Celularazul
+  puts("Prueba de gema")
+end
+
+lambda
---
+++ lib/celularazul/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Celularazul
+  VERSION = "0.1.0"
+end
